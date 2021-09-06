918635f6c329df8970cb77fdbfa4ff31bca904fa
Mikita <ch8ch8k@gmail.com>
2021-09-06 14:01:19 +0300

    test commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {List1Component} from "./list1/list1.component";
+import {List2Component} from "./list2/list2.component";
+import {HomeComponent} from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'list1', component: List1Component},
+  {path: 'list2', component: List2Component},
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { List1Component } from './list1/list1.component';
+import { List2Component } from './list2/list2.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    List1Component,
+    List2Component,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
